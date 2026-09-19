4710f7846c0c207fa72e771d8e01638863d2c22c
Abrar <abrarshekh822@gmail.com>
2026-09-20 00:01:55 +0530

    add auth

--- backend/auth/auth_endpoints.py
+++ backend/auth/auth_endpoints.py
@@ -3,14 +3,171 @@ from ..pydantic_models.schemas import AdminSignup, UserSignup, Login
 from ..db_operations.db_connect import db_connect
 from psycopg2.extras import RealDictCursor
 from .security import hash_password, verify_password
+from .jwt import create_jwt
 
 db_endpoints = APIRouter()
 
 
 @db_endpoints.post("/admin_signup", status_code=status.HTTP_201_CREATED)
 def admin_signup(admin: AdminSignup, conn=Depends(db_connect)):
-    pass
+    tupl = (
+        admin.organization_name,
+        admin.code,
+        admin.email,
+        hash_password(admin.password),
+    )
+    cur = None
+    try:
+        cur = conn.cursor(cursor_factory=RealDictCursor)
 
-@db_endpoints.post('/user_signup')
-def user_signup(user: UserSignup, conn = Depends(db_connect)):
-    pass
+        # checking the user exists
+        query = """SELECT "Name" FROM "Organization" WHERE "Email" = %s"""
+        cur.execute(query, (admin.email,))
+        result = cur.fetchone()
+        if result["Name"]:
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT, detail=f"The user already exists."
+            )
+
+        query = """INSERT INTO "Organization" ("Name", "Code", "Email", "Password") VALUES (%s, %s, %s, %s)"""
+        cur.execute(query, tupl)
+        conn.commit()
+        return {"message": "Admin registered successfully."}
+    except HTTPException as httpErr:
+        raise httpErr
+    except Exception as err:
+        if conn:
+            conn.rollback()
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Some Error Occurred" + str(err),
+        )
+    finally:
+        if cur:
+            cur.close()
+
+
+@db_endpoints.post("/user_signup", status_code=status.HTTP_201_CREATED)
+def user_signup(user: UserSignup, conn=Depends(db_connect)):
+    cur = None
+    try:
+        cur = conn.cursor(cursor_factory=RealDictCursor)
+
+        # Verifying the code
+        query = """SELECT "O_id" from "Organization" WHERE "Code" = %s"""
+        cur.execute(query, (user.code,))
+        result = cur.fetchone()
+        if result:
+            o_id = result["O_id"]
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail=f"Registration failed: Invalid or unrecognized organization invitation code.",
+            )
+
+        # check the user exists
+        query = """SELECT "Name" FROM "Organization" WHERE "Email" = %s"""
+        cur.execute(query, (user.email,))
+        result = cur.fetchone()
+        if result["Name"]:
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT, detail=f"The user already exists."
+            )
+
+        tupl = (o_id, user.email, user.phone_number, user.password)
+        query = """INSERT INTO "User" ("O_id", "Email", "Phone_Number", "Password") 
+            VALUES (%s, %s, %s, %s);"""
+        cur.execute(query, tupl)
+        conn.commit()
+        return {"message": "User registered successfully."}
+    except HTTPException as httpErr:
+        raise httpErr
+    except Exception as err:
+        if conn:
+            conn.rollback()
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Some Error Occurred" + str(err),
+        )
+    finally:
+        if cur:
+            cur.close()
+
+
+@db_endpoints.post("/admin_login", status_code=status.HTTP_200_OK)
+def admin_login(admin: Login, conn=Depends(db_connect)):
+    cur = None
+    try:
+        cur = conn.cursor(cursor_factory=RealDictCursor)
+
+        # retrieving the password
+        query = """SELECT "O_id", "Password" FROM "Organization" WHERE "Email" = %s"""
+        cur.execute(query, (admin.email,))
+        result = cur.fetchone()
+        if not result:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="The Entered credentials are invalid.",
+            )
+
+        stored_hash = result["Password"]
+        if verify_password(admin.password, stored_hash):
+            o_id = result["O_id"]
+            payload = {"o_id": o_id}
+            token = create_jwt(payload)
+            return {"access_token": token, "token_type": "bearer"}
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="The Entered credentials are invalid.",
+            )
+    except HTTPException as httpErr:
+        raise httpErr
+    except Exception as err:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Some Error Occurred" + str(err),
+        )
+    finally:
+        if cur:
+            cur.close()
+
+
+@db_endpoints.post("/user_login", status_code=status.HTTP_200_OK)
+def user_login(user: Login, conn=Depends(db_connect)):
+    cur = None
+    try:
+        cur = conn.cursor(cursor_factory=RealDictCursor)
+
+        # checking if the user exists
+        query = """SELECT "U_id", "Password" FROM "User" WHERE "Email" = %s"""
+        cur.execute(query, (user.email,))
+        result = cur.fetchone()
+        if result:
+            stored_password = result["Password"]
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="The Entered Credentials are invalid.",
+            )
+
+        if verify_password(user.password, stored_password):
+            u_id = result["U_id"]
+            payload = {"u_id": u_id}
+            token = create_jwt(payload)
+            return {"access_token": token, "token_type": "bearer"}
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="The Entered Credentials are invalid.",
+            )
+    except HTTPException as httpErr:
+        raise httpErr
+    except Exception as err:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Some Error Occurred" + str(err),
+        )
+    finally:
+        if cur:
+            cur.close()
---
+++ backend/auth/jwt.py
@@ -0,0 +1,60 @@
+from dotenv import load_dotenv
+import os
+from datetime import datetime, timedelta, timezone
+import jwt
+
+load_dotenv(dotenv_path="./backend/.env")
+jwt_algorithm = os.getenv("JWT_ALGORITHM")
+secret_key = os.getenv("JWT_SECRET_KEY")
+
+
+def create_jwt(payload: dict):
+    to_encode = payload.copy()
+    exp = datetime.now(timezone.utc) + timedelta(minutes=2)
+    to_encode.update({"exp": exp})
+
+    encoded_jwt = jwt.encode(to_encode, secret_key, jwt_algorithm)
+    return encoded_jwt
+
+
+from fastapi.security import OAuth2PasswordBearer
+from fastapi import Depends, HTTPException, status
+
+oauth_scheme_user = OAuth2PasswordBearer(tokenUrl="/user_login")
+
+
+def get_u_id(token=Depends(oauth_scheme_user)):
+    try:
+        payload = jwt.decode(token, secret_key, algorithms=[jwt_algorithm])
+        u_id = payload.get("u_id")
+        if u_id:
+            return u_id
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid JWT Token."
+            )
+    except jwt.PyJWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Session has expired or token is invalid.",
+        )
+
+
+oauth_scheme_admin = OAuth2PasswordBearer(tokenUrl="/admin_login")
+
+
+def get_o_id(token=Depends(oauth_scheme_admin)):
+    try:
+        payload = jwt.decode(token, secret_key, algorithms=[jwt_algorithm])
+        o_id = payload.get("o_id")
+        if o_id:
+            return o_id
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid JWT Token."
+            )
+    except jwt.PyJWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Session has expired or token is invalid.",
+        )
